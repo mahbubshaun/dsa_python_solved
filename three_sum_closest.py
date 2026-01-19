fc1ec29faf63b3234f4a1ee2be0060496fd990ba
mahbubshaun <mdmahbubshaun@gmail.com>
2026-01-19 22:27:52 +0600

    three sum closest attempt 2

--- three_sum_closest.py
+++ three_sum_closest.py
@@ -5,22 +5,29 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         n = len(nums)
         nums.sort()
-        closest_sum = 0
+        max_closest_sum = float('inf')
+        current_closest = 0
         for i in range(n):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             j = i+1
             k = n-1
             while j< k:
-                closest_sum = abs(target - (nums[j] + nums[k] + nums[i]))
+                current_sum = nums[j] + nums[k] + nums[i]
+                if current_sum != 0:
+                    current_closest = abs(target - current_sum)
 
 
-                if closest_sum > target:
+                if current_closest < max_closest_sum:
+                    max_closest_sum = current_closest
+
+
+                if max_closest_sum > target:
                     k -= 1
 
                 else:
                     j += 1
-        return closest_sum
+        return max_closest_sum
 
 nums = [-1,2,1,-4]
 target = 9
